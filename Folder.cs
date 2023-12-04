375c368226c9bd48ced74961422c74e9b50ed14a
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-12-04 23:54:57 +0500

    Добавьте файлы проекта.

---
+++ Folder.cs
@@ -0,0 +1,53 @@
+﻿namespace FolderSizeCalculator;
+
+internal class Folder
+{
+    internal double GetSize(string Path)
+    {
+        double size = 0;
+        try
+        {
+            var directory = new DirectoryInfo(Path);
+            if (directory.Exists)
+            {
+                foreach (DirectoryInfo dir in directory.GetDirectories())
+                {
+                    try
+                    {
+                        size += GetSize(dir.FullName);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Недопустимое действие или недостаточно прав на выполнение для {dir.FullName}.");
+                        continue;
+                    }
+                }
+                FileInfo[] files = directory.GetFiles();
+                if (files.Length > 0)
+                {
+                    foreach (FileInfo file in files)
+                    {
+                        try
+                        {
+                            size += file.Length;
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            Console.WriteLine($"Недопустимое действие или недостаточно прав на выполнение для {file.FullName}.");
+                            continue;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Указанный каталог {Path} не существует");
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return size;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FolderSizeCalculator;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к каталогу:");
+        string? Path = Console.ReadLine();
+        if (!string.IsNullOrEmpty(Path) && !string.IsNullOrWhiteSpace(Path))
+        {
+            try
+            {
+                Folder folder = new();
+                double folderSize = folder.GetSize(Path);
+                Console.WriteLine($"Размер каталога: {folderSize} байт.");
+                Console.ReadKey();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Недопустимая часть пути к каталогу.");
+            }
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("Путь или имя файла превышает максимальную длину, определенную системой.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Путь к каталогу не указан.");
+        }
+    }
+}
